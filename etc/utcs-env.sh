a30be743c5fe8e8ece99e0c69e5a8d6cdf9e8356
jack.oquin <jack.oquin@f2b30410-025d-11df-9f77-c71dee008b03>
2010-01-19 18:59:34 +0000

    update README, delete ROS_STACK_PATH which is no longer needed

--- etc/utcs-env.sh
+++ etc/utcs-env.sh
@@ -8,9 +8,9 @@ ln -s /lusr/opt/ros-28450 shared_ros
 ln -s shared_ros/installed/ros .
 ln -s shared_ros/installed/pkgs .
 
-# Copy ROS environment initialization commands to stdout these are
-# normally appended to .bashrc.  The EOF is quoted, so none of the
-# lines that follow will be expanded.
+# Copy ROS environment initialization commands to stdout, these are
+# normally appended to .bashrc (or to some file sourced there).  The
+# EOF is quoted, so none of the lines that follow will be expanded.
 cat <<'EOF'
 export ROS_ROOT=$HOME/ros/ros
 export PATH=$ROS_ROOT/bin:${PATH}
@@ -20,7 +20,6 @@ if [ ! "$ROS_MASTER_URI" ]
 then   export ROS_MASTER_URI=http://localhost:11311
 fi
 export ROS_PACKAGE_PATH=$HOME/ros/pkgs:$HOME/svn/utexas-art-ros-pkg
-export ROS_STACK_PATH=$ROS_ROOT:$HOME/ros/pkgs:$HOME/svn/utexas-art-ros-pkg
 
 source $ROS_ROOT/tools/rosbash/rosbash        
 EOF

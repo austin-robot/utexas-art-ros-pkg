a586d9154c9a2fd77419bba276062d4501097651
jack.oquin <jack.oquin@f2b30410-025d-11df-9f77-c71dee008b03>
2010-08-27 01:14:08 +0000

    add C++ copyright templates

---
+++ etc/copyright.cc
@@ -0,0 +1,9 @@
+/*
+ *  Finite state machine implementation
+ *
+ *  Copyright (C) 2010, Austin Robot Technology
+ *
+ *  License: Modified BSD Software License Agreement
+ *
+ *  $Id$
+ */
---
+++ etc/copyright.h
@@ -0,0 +1,10 @@
+/* -*- mode: C++ -*-
+ *
+ *  Finite state machine interface
+ *
+ *  Copyright (C) 2010, Austin Robot Technology
+ *
+ *  License: Modified BSD Software License Agreement
+ *
+ *  $Id$
+ */
